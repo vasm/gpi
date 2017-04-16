036859e383a4c68d864ca4cbfc99379ada3addde
Vasiliy Sabadazh <vsabadazh@gmail.com>
2017-04-16 22:13:32 +0300

    cleaned .ycm_extra_conf.py

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,20 +1,7 @@
-# For God's sake, don't forget to rename this .ycm_extra_conf.py
-
-# Note the leading dot!  I downloaded this, configured it endlessly and nothing
-# seemed to improve...Only to find out I didn't put the dot in the beginning because it got replaced
-# with an underscore by my browser...Maybe I'm an idiot, but don't let this happen to you!
-
-
 import os
 import ycm_core
 from clang_helpers import PrepareClangFlags
 
-# Set this to the absolute path to the folder (NOT the file!) containing the
-# compile_commands.json file to use that instead of 'flags'. See here for
-# more details: http://clang.llvm.org/docs/JSONCompilationDatabase.html
-# Most projects will NOT need to set this to anything; you can just change the
-# 'flags' list of compilation flags. Notice that YCM itself uses that approach.
-compilation_database_folder = ''
 
 # These are the compilation flags that will be used in case there's no
 # compilation database set.
@@ -59,66 +46,7 @@ flags = [
 # This path is for AVR gcc. 
 ]
 
-"""
-if compilation_database_folder:
-  database = ycm_core.CompilationDatabase( compilation_database_folder )
-else:
-  database = None
-
-
-def DirectoryOfThisScript():
-  return os.path.dirname( os.path.abspath( __file__ ) )
-
-
-def MakeRelativePathsInFlagsAbsolute( flags, working_directory ):
-  if not working_directory:
-    return flags
-  new_flags = []
-  make_next_absolute = False
-  path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
-  for flag in flags:
-    new_flag = flag
-    if make_next_absolute:
-      make_next_absolute = False
-      if not flag.startswith( '/' ):
-        new_flag = os.path.join( working_directory, flag )
-
-    for path_flag in path_flags:
-      if flag == path_flag:
-        make_next_absolute = True
-        break
-
-      if flag.startswith( path_flag ):
-        path = flag[ len( path_flag ): ]
-        new_flag = path_flag + os.path.join( working_directory, path )
-        break
-
-    if new_flag:
-      new_flags.append( new_flag )
-  return new_flags
-  """
-
 
 def FlagsForFile( filename ):
     return {'flags': flags}
-    """
-    if database:
-    # Bear in mind that compilation_info.compiler_flags_ does NOT return a
-    # python list, but a "list-like" StringVec object
-    compilation_info = database.GetCompilationInfoForFile( filename )
-    final_flags = PrepareClangFlags(
-        MakeRelativePathsInFlagsAbsolute(
-            compilation_info.compiler_flags_,
-            compilation_info.compiler_working_dir_ ),
-        filename )
-
-      else:
-        relative_to = DirectoryOfThisScript()
-        final_flags = MakeRelativePathsInFlagsAbsolute( flags, relative_to )
-
-      return {
-        'flags': final_flags,
-        'do_cache': True
-      }
-      """
 
